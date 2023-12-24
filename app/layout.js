2d3e97b1a54bdd452c085f4f1b9012a379f71b82
zohre-dev <mehrabi.z13@gmail.com>
2023-12-25 01:43:49 +0200

    build:attached bYekan font to body tag

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,16 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+const myfont = localFont({
+  src: [
+    {
+      path: '../public/assets/fonts/bYekanRegular.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-byekan',
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +19,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fa" dir="rtl" className={myfont.className}>
+      <body className={`${myfont.variable}`}>{children}</body>
     </html>
   )
 }
